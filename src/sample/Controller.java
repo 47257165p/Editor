9454463db3b15d1858bc05cbae5e879b5f2d4364
Alejandro <47257165p@giespoblenou.org>
2015-11-06 19:14:17 +0100

    Commit final. Finalizado el editor de texto con todas las funciones implementadas (botones funcionales, menú con varias opciones como cambio de estilo de letra, tamaño de letra, abrir archivo, guardar archivo, etc). -Alejandro Soriano

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,102 +1,139 @@
 package sample;
 
+import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import javax.xml.soap.Text;
+import java.io.*;
 
 public class Controller {
-    public TextArea text;
-    public MenuItem miOFTimes;
-    public MenuItem miOFArial;
-    public MenuItem miOT12;
-    public MenuItem miOT10;
-    public MenuItem miFSortir;
-    public MenuItem miECopiar;
-    public MenuItem miETallar;
-    public MenuItem miEPaste;
-    public MenuItem miEUndo;
-    public Button btCopy;
-    public MenuItem miAAbout;
-    public AnchorPane mainPane;
+
+
+    public MenuItem mIMono;
+    public MenuItem mISerif;
+    public MenuItem mIT14;
+    public MenuItem mIT12;
+    public MenuItem mISalir;
+    public MenuItem mICopiar;
+    public MenuItem mICortar;
+    public MenuItem mIPegar;
+    public MenuItem mIDeshacer;
+    public MenuItem mIAbrir;
+    public MenuItem mIGuardar;
+    public MenuItem mIInfo;
+    public Button bTCopiar;
+    public Button btPegar;
+    public Button btCortar;
+    public TextArea texto;
 
     private double fontSize;
 
-    /**
-     * S'executa al iniciar l'aplicació.
-     */
+
     public void initialize(){
-        fontSize = text.getFont().getSize();
-        btCopy.setGraphic(new ImageView("copy.png"));
+        fontSize = texto.getFont().getSize();
+        bTCopiar.setGraphic(new ImageView("copy.png"));
+        btPegar.setGraphic(new ImageView("paste.png"));
+        btCortar.setGraphic(new ImageView("cut.png"));
     }
 
-    /**
-     * Menú Opcions -> Font. Canvi de la font del text.
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void setFont(ActionEvent actionEvent) {
-        text.setFont(new Font(((MenuItem) actionEvent.getSource()).getText(), fontSize));
+    public void setFuente(ActionEvent actionEvent) {
+        texto.setFont(new Font(((MenuItem) actionEvent.getSource()).getText(), fontSize));
     }
 
-    /**
-     * Menú Opcions -> Tamany. Canvi del tamany de la font del text.
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void setFontSize(ActionEvent actionEvent) {
+    public void setTamaño(ActionEvent actionEvent) {
         fontSize = Double.parseDouble(((MenuItem)actionEvent.getSource()).getText());
-        text.setFont(new Font(fontSize));
+        texto.setFont(new Font(fontSize));
     }
 
-    /**
-     * Menú Fitxer -> Sortir. Surt de l'aplicació
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void sortir(ActionEvent actionEvent) {
+    public void onSalir(ActionEvent actionEvent) {
         Platform.exit();
     }
 
-    /**
-     * Menú Editar -> Copiar. Còpia el text seleccionat.
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void eCopiar(ActionEvent actionEvent) {
-        text.copy();
+    public void onCopiar(ActionEvent actionEvent) {
+        texto.copy();
     }
 
-    /**
-     * Menú Editar -> Tallar. Talla el text seleccionat.
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void eTallar(ActionEvent actionEvent) {
-        text.cut();
+    public void onCortar(ActionEvent actionEvent) {
+        texto.cut();
     }
 
-    /**
-     * Menú Editar -> Enganxar. Enganxa el text del clipboard.
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void ePaste(ActionEvent actionEvent) {
-        text.paste();
+    public void onPegar(ActionEvent actionEvent) {
+        texto.paste();
     }
 
-    /**
-     * Menú Editar -> Desfer. Desfà la última acció.
-     * @param actionEvent Event onAction de tots els MenuItem
-     */
-    public void eUndo(ActionEvent actionEvent) {
-        text.undo();
+    public void onDeshacer(ActionEvent actionEvent) {
+        texto.undo();
     }
 
-    public void eAbout(ActionEvent actionEvent) {
+    public void onInfo(ActionEvent actionEvent) {
+
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Information Dialog");
-        alert.setHeaderText("Look, an Information Dialog");
-        alert.setContentText("I have a great message for you!");
-
+        alert.setTitle("Información sobre el programa");
+        alert.setHeaderText("Aquí tiene la información sobre el programa");
+        alert.setContentText("Editor de texto simple.");
         alert.showAndWait();
+    }
+
+    public void onAbrir(ActionEvent actionEvent) {
+
+        FileChooser fchooser = new FileChooser();
+        fchooser.setTitle("Abrir archivo");
+        fchooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Archivos de texto", "*.txt"),
+                new FileChooser.ExtensionFilter("Todos los archivos", "*")
+        );
+        Stage stage = new Stage();
+        File fileChoosed = fchooser.showSaveDialog(stage);
+        fchooser.setTitle(fileChoosed.getName());
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(fileChoosed));
+            while (br.ready())
+            {
+                texto.setText(texto.getText()+br.readLine()+"\n");
+            }
+            br.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGuardar(ActionEvent actionEvent) {
+
+        FileChooser fchooser = new FileChooser();
+        fchooser.setTitle("Abrir archivo");
+        fchooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Archivos de texto", "*.txt"),
+                new FileChooser.ExtensionFilter("Todos los archivos", "*")
+        );
+        Stage stage = new Stage();
+        File fileChoosed = fchooser.showSaveDialog(stage);
+        fchooser.setTitle(fileChoosed.getName());
+
+        try
+        {
+            fileChoosed.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileChoosed));
+            bw.write(texto.getText());
+            bw.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
